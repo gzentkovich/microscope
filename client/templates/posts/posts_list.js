cd0ca319a9fb323558e7afa04cb11191ec25888b
gzentkovich <gzentkovich@gmail.com>
2014-12-31 23:58:44 -1000

    mod posts_list.js to read from db

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,17 +1,26 @@
-var postsData = [
-{
-  title: 'Introducing Telescope',
-  url: 'http://sachagreif.com/introducing-telescope/'
-},
-{
-  title: 'Meteor',
-  url: 'http://meteor.com'
-},
-{
-  title: 'The Meteor Book',
-  url: 'http://themeteorbook.com'
-}
-];
+// var postsData = [
+// {
+//   title: 'Introducing Telescope',
+//   url: 'http://sachagreif.com/introducing-telescope/'
+// },
+// {
+//   title: 'Meteor',
+//   url: 'http://meteor.com'
+// },
+// {
+//   title: 'The Meteor Book',
+//   url: 'http://themeteorbook.com'
+// }
+// ];
+// Template.postsList.helpers({
+//   posts: postsData
+// });
+// Sample Static Data
+
+
+// From the Database
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
